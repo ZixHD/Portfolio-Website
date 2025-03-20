890ebbddd4b4285d4c663307101bcc6f79ab1054
ZixHD <havos25@gmail.com>
2025-03-20 16:19:26 +0100

    About-me component small change

--- my-website/components/AboutMe/AboutMe.js
+++ my-website/components/AboutMe/AboutMe.js
@@ -8,12 +8,12 @@ export default function AboutMe() {
                 <p className="text">
                     I'm a 22-year-old experienced Full Stack Developer with a knack for building products that make a difference. My expertise spans both frontend and backend technologies, enabling me to craft comprehensive solutions from the ground up.
                 </p>
-                <p className="text">
+                {/* <p className="text">
                     Throughout my career, I've been deeply involved in the startup ecosystem. I've worked with various startups, embracing the fast-paced, innovative nature of these environments. This journey led me to found my own startup, where I gained firsthand experience in every aspect of product development and business operations.
                 </p>
                 <p className="text">
                     During my high school years, I excelled in software development, earning the prestigious title of DUX (top student) in this field. This early achievement laid the foundation for my passion and expertise in programming.
-                </p>
+                </p> */}
                 <p className="text">
                     When I'm not immersed in code, you'll find me creating games in the Godot engine, reading books or drawing. I'm always creating, whether it's digital products or tangible objects.
                 </p>
